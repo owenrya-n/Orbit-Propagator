c634d867f21eedcd99b98c6306b154b33c688130
Metastable1883 <owenryan1883@gmail.com>
2022-10-19 22:54:48 -0700

    a

--- MAIN.m
+++ MAIN.m
@@ -1,4 +1,4 @@
-filename = 'test.csv'
+filename = 'tb.csv'
 M = csvread(filename);
 figure()
 plot3Dbody('earthImage.Jpg',6371,0)
@@ -6,7 +6,7 @@ hold on
 x=M(1,:);
 y=M(2,:);
 z=M(3,:);
-plot3(x(1:1:end), y(1:1:end), z(1:1:end))
+plot3(x(1:3:end), y(1:3:end), z(1:3:end))
 xlabel('x [m]')
 ylabel('y [m]')
 zlabel('z [m]')
